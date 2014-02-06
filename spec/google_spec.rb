34cf4fbf980db5c05681a3351186c0111ef1ba3b
Mike Gehard <mike.gehard@gmail.com>
2014-02-06 15:46:45 -0700

    Add in other capybara syntax for specs

--- spec/google_spec.rb
+++ spec/google_spec.rb
@@ -12,3 +12,14 @@ feature "Performing a google search", js: true do
     expect(page).to have_content("Jeff Dean (mikegehard) on Twitter")
   end
 end
+
+# same as above test, just different verbiage
+describe "Performing a google search", type: :feature, js: true do
+  it "allows to search for Mike Gehard" do
+    visit "/"
+    fill_in "q", with: "Mike Gehard"
+
+    expect(page).to have_content("Mike Gehard (mikegehard) on Twitter")
+    expect(page).to have_content("Jeff Dean (mikegehard) on Twitter")
+  end
+end
